215dac440f8d91aa8176f162a8b917c09ec37223
Sridhar Maskeri <shridharmaskeri@gmail.com>
2025-01-26 09:33:19 +0530

    Updated the customer details query hook

--- features/customers/api/use-get-customer-details.ts
+++ features/customers/api/use-get-customer-details.ts
@@ -3,14 +3,12 @@ import { useQuery } from "@tanstack/react-query";
 
 export const useGetCustomerDetails = ({ id }: { id: string }) => {
   const query = useQuery({
-    queryKey: ["customerDetails"],
+    queryKey: ["customers", id],
     queryFn: async () => {
       const customer = await APIService.getCustomerById({ id });
 
       return customer;
     },
-    staleTime: 0,
-    enabled: !!id,
   });
 
   return query;
